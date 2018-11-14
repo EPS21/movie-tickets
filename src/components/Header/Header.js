3419bea7b6c94fe10033b9b8372e4ad7f35f39e7
David Gonzalez <oneproductions12@gmail.com>
2018-11-14 10:54:27 -0800

    Added routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Header.css'
+import cart from '../imgs/cart.png'
 
 export default class Header extends React.Component{
     render(){
         return(
-            <h1>Fandango Rip-off</h1>
+            <div className="Head">
+                <h1>Fandango Rip-off</h1>
+                <div className="Carts">
+                    <p>Welcome, User</p>
+                    <img src={cart} alt={cart} height="35" width="35"/>
+                </div>
+            </div>
         )
     }
 }
