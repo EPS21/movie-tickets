89f84ae3d2c27455b0989b3282dc44b204d01e02
David Gonzalez <oneproductions12@gmail.com>
2018-11-15 10:38:36 -0800

    Made changes in the Cart.js. Trying to figure out how to pass the movie info to the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Purchase from './Purchase';
+import ShortDesc from './ShortDesc';
 
 export default class Cart extends React.Component{
     render(){
+        const movie = this.props.movie
+        console.log(movie)
         return(
-            <div>
-                <h3>Cart goes here</h3>
+            <div className="cart-container">
+                <Purchase />
+                <ShortDesc movie={movie}/>
             </div>
         )
     }
--- src/components/Cart/Purchase.js
+++ src/components/Cart/Purchase.js
@@ -1,1 +1,9 @@
 import React from 'react';
+
+export default class Purchase extends React.Component{
+    render(){
+        return(
+            <h3>Purchases goes here</h3>
+        )
+    }
+}
